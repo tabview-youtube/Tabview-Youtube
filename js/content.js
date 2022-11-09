42e4559bf1069194aedba176b333eeefc5346ef0
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2022-11-09 20:56:10 +0900

    v2.12.2
    v2.12.2 - added Simplified Chinese

--- js/content.js
+++ js/content.js
@@ -2964,6 +2964,12 @@
       'videos':'影片',
       'playlist':'播放清單'
     },
+    'cn':{
+      'share':'分享',
+      'info':'资讯',
+      'videos':'视频',
+      'playlist':'播放清单'
+    },
     'du':{
       'share':'Teilen',
       'info':'Info',
